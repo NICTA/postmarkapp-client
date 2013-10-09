70b00dda23467198b90c5f21b4c6efc4f3e56e4a
Sidney Shek <sidney.shek@nicta.com.au>
2013-10-10 09:51:35 +1100

    Removed unnecessary test grouping as issue has been fixed in SBT

--- project/Build.scala
+++ project/Build.scala
@@ -9,16 +9,4 @@ object B extends Build
     Project("root", file("."))
       .configs( CustomIntegrationTest )
       .settings( inConfig(CustomIntegrationTest)(Defaults.testSettings) : _*)
-      .settings(
-      // FIX - Remove dummy test group as a fix for: https://github.com/sbt/sbt/issues/539
-      testGrouping in CustomIntegrationTest <<= definedTests in CustomIntegrationTest map partitionTests("integrationTests"),
-      testGrouping in Test <<= definedTests in Test map partitionTests("unitTests"),
-      parallelExecution in CustomIntegrationTest := false
-    )
-
-  def partitionTests(groupName: String)(tests: Seq[TestDefinition]) = {
-    Seq(
-      new Group(groupName, tests, SubProcess(Seq()))
-    )
-  }
 }
