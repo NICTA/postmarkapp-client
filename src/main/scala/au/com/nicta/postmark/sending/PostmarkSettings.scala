c3238e75193513c048877b785bc03760a4854d80
Sidney Shek <sidney.shek@nicta.com.au>
2013-10-10 09:08:42 +1100

    Added test connections using Postmark's test token

--- src/main/scala/au/com/nicta/postmark/sending/PostmarkSettings.scala
+++ src/main/scala/au/com/nicta/postmark/sending/PostmarkSettings.scala
@@ -4,6 +4,9 @@ package sending
 case class PostmarkSettings(apiUrl: String, apiToken: String)
 
 object PostmarkSettings {
+  val TEST_TOKEN = "POSTMARK_API_TEST"
   def http(token: String) = PostmarkSettings("http://api.postmarkapp.com", token)
   def https(token: String) = PostmarkSettings("https://api.postmarkapp.com", token)
+  def httpTest = http(TEST_TOKEN)
+  def httpsTest = https(TEST_TOKEN)
 }
